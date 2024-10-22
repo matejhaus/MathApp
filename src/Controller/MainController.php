be42c90a7cf367d9603b561c344a82cc8ab8e293
matejhaus <matej.hauschwitz@seznam.cz>
2024-10-22 18:24:34 +0200

    fix HP show if user is loggouted

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,25 +2,29 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Entity\UserStatistics;
 use App\Repository\QuotesRepository;
 use App\Repository\ThemeRepository;
-use App\Repository\UserRepository;
 use App\Repository\UserStatisticsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(ThemeRepository $themeRepository, QuotesRepository $quotesRepository, UserInterface $user, UserStatisticsRepository $userStatistics): Response
+    public function index(ThemeRepository $themeRepository, QuotesRepository $quotesRepository, UserStatisticsRepository $userStatistics, Security $security): Response
     {
+        $user = $security->getUser();
+
         $themes = $themeRepository->findAllThemes();
         $quotes = $quotesRepository->findBy([], ['position' => 'ASC']);
-        $statsCount = $userStatistics->countStatisticsByUser($user);
+
+        if ($user) {
+            $statsCount = $userStatistics->countStatisticsByUser($user);
+        } else {
+            $statsCount = null;
+        }
 
         return $this->render('main/index.html.twig', [
             'quotes' => $quotes,
